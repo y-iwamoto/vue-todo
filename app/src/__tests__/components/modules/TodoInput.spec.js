55489e991a8002b38621e953169fb0b0a08defe7
yuki-iwamoto <yuki_iwamoto@genesis-healthcare.jp>
2021-06-20 21:04:52 +0900

    Todo入力フォームのtestを作成

---
+++ app/src/__tests__/components/modules/TodoInput.spec.js
@@ -0,0 +1,45 @@
+import { shallowMount } from '@vue/test-utils'
+import Component from '@/components/modules/TodoInput.vue'
+
+describe('Testing TodoInput component', () => {
+  const inputText = 'test1'
+  const errorText = 'TODOは未入力です'
+
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallowMount(Component)
+  })
+
+  afterEach(() => {
+    wrapper.destroy()
+  })
+
+  it('Initial rendering', () => {
+    expect(wrapper.text()).toContain('新規タスクの追加')
+    expect(wrapper.text()).toContain('追加')
+    expect(wrapper.text()).not.toContain(errorText)
+  })
+
+  it('The input form is empty', async () => {
+    await wrapper.get('input').setValue('')
+    await wrapper.find('form').trigger('submit.prevent')
+    expect(wrapper.emitted('addTodo')).toBe(undefined)
+    expect(wrapper.text()).toContain(errorText)
+  })
+
+  it('The input form is filled out', async () => {
+    await wrapper.setData({
+      error: errorText,
+    })
+    expect(wrapper.text()).toContain(errorText)
+
+    await wrapper.get('input').setValue(inputText)
+    expect(wrapper.vm.$data.todo).toBe(inputText)
+
+    await wrapper.find('form').trigger('submit.prevent')
+    expect(wrapper.emitted('addTodo')[0][0]).toBe(inputText)
+    expect(wrapper.vm.$data.todo).toBe('')
+    expect(wrapper.text()).not.toContain(errorText)
+  })
+})
