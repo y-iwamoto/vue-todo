801a0073b6a20a16b8925463497690deffd41d74
yuki-iwamoto <yuki_iwamoto@genesis-healthcare.jp>
2021-06-22 22:50:34 +0900

    getters.jsのテスト作成

---
+++ app/src/__tests__/store/getter.spec.js
@@ -0,0 +1,47 @@
+import getters from '@/store/getters'
+describe('getters', () => {
+  const todos = [
+    { id: 0, comment: 'test1', status: '作業中' },
+    { id: 1, comment: 'test2', status: '完了' },
+    { id: 2, comment: 'test3', status: '作業中' },
+  ]
+
+  it('getLatestTodo', () => {
+    const state = {
+      todos: todos,
+    }
+    const result = getters.getLatestTodo(state)
+
+    expect(result).toEqual({ id: 2, comment: 'test3', status: '作業中' })
+  })
+
+  it('getFilter', () => {
+    const state = {
+      filter: '全て',
+      todos: [],
+    }
+    const result = getters.getFilter(state)
+
+    expect(result).toEqual('全て')
+  })
+
+  describe('getTodos', () => {
+    it('filter value is 全て', () => {
+      const state = {
+        todos: todos,
+      }
+      const result = getters.getTodos(state)('全て')
+
+      expect(result).toEqual(todos)
+    })
+
+    it('filter value is with the exception of 全て', () => {
+      const state = {
+        todos: todos,
+      }
+      const result = getters.getTodos(state)('完了')
+      expect(result).toHaveLength(1)
+      expect(result[0]).toEqual(todos[1])
+    })
+  })
+})
